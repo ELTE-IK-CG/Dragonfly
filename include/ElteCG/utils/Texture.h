c6446ab65bfbee958a473683d0060b64e0cd46da
Róbert Bán <robert.ban102@gmail.com>
2019-10-25 04:07:32 +0200

    Texture2D load from file done, needs testing on a big endian system

--- include/ElteCG/utils/Texture.h
+++ include/ElteCG/utils/Texture.h
@@ -34,6 +34,43 @@ namespace detail
 			return false;
 		}
 	}
+
+	inline int invert_image(int pitch, int height, void* image_pixels)
+	{
+		int index;
+		void* temp_row;
+		int height_div_2;
+
+		temp_row = (void *)malloc(pitch);
+		if (NULL == temp_row)
+		{
+			SDL_SetError("Not enough memory for image inversion");
+			return -1;
+		}
+		//if height is odd, don't need to swap middle row
+		height_div_2 = (int)(height * .5);
+		for (index = 0; index < height_div_2; index++) {
+			//uses string.h
+			memcpy((Uint8 *)temp_row,
+				(Uint8 *)(image_pixels)+
+				pitch * index,
+				pitch);
+
+			memcpy(
+				(Uint8 *)(image_pixels)+
+				pitch * index,
+				(Uint8 *)(image_pixels)+
+				pitch * (height - index - 1),
+				pitch);
+			memcpy(
+				(Uint8 *)(image_pixels)+
+				pitch * (height - index - 1),
+				temp_row,
+				pitch);
+		}
+		free(temp_row);
+		return 0;
+	}
 }
 
 class TextureLowLevelBase {
@@ -66,6 +103,8 @@ protected:
 		constexpr GLenum iFormat = eltecg::ogl::helper::getInternalFormat<InternalFormat>();
 		static_assert(sizeof(InternalFormat) == sizeof(NewInternalFormat), "Texture: Internal formats must be of the same size class");
 		glTextureView(view.texture_id, static_cast<GLenum>(NewTexType), this->texture_id, iFormat, minLevel, numLevels, minLayer, numLayers);
+		GL_CHECK;
+		return view;
 	}
 public:
 	inline void bind() const { glBindTexture(static_cast<GLenum>(TexType), this->texture_id); }
@@ -107,7 +146,7 @@ public:
 		return this->_MakeView<NewInternalFormat, NewTexType>(minLevel, numLevels, 0, 1);
 	}
 
-	Texture(const std::string &file, int levels = -1)
+	Texture(const std::string &file, bool invertImage = true, int levels = -1)
 	{
 		SDL_Surface* loaded_img = IMG_Load(file.c_str());
 		ASSERT(loaded_img != nullptr, ("Texture2D: Failed to load texture from \"" + file + "\".").c_str());
@@ -115,8 +154,20 @@ public:
 		if(levels == -1) levels = static_cast<int>(floor(log2(loaded_img->w > loaded_img->h ? loaded_img->w : loaded_img->h))) + 1;
 		InitTexture(loaded_img->w, loaded_img->h, levels);
 
-		GLenum sdl_channels = SDL_BYTEORDER == SDL_LIL_ENDIAN ? (loaded_img->format->BytesPerPixel == 4 ? GL_BGRA : GL_BGR) : (loaded_img->format->BytesPerPixel == 4 ? GL_RGB : GL_RGB);
 		GLenum sdl_pxformat = GL_UNSIGNED_BYTE; //todo calculate from format
+		Uint32 sdl_format = loaded_img->format->BytesPerPixel == 3 ? SDL_PIXELFORMAT_RGB24 : SDL_PIXELFORMAT_RGBA32;
+		GLenum sdl_channels = loaded_img->format->BytesPerPixel == 3 ? GL_RGB : GL_RGBA;
+		if (loaded_img->format->format != sdl_format) {
+			SDL_Surface* formattedSurf = SDL_ConvertSurfaceFormat(loaded_img, sdl_format, 0);
+			SDL_FreeSurface(loaded_img);
+			ASSERT(formattedSurf != nullptr, "Texture2D: Failed to convert texture type.");
+			loaded_img = formattedSurf;
+		}
+
+		if (invertImage) {
+			int ret = detail::invert_image(loaded_img->pitch, loaded_img->h, loaded_img->pixels);
+			ASSERT(ret == 0, "Texture2D: Failed to invert image");
+		}
 
 		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, this->_width, this->_height, sdl_channels, sdl_pxformat, static_cast<void*>(loaded_img->pixels));
 
@@ -153,12 +204,12 @@ public:
 	template<TextureType NewTexType = TextureType::TEX_CUBE_MAP, typename NewInternalFormat = InternalFormat>
 	auto MakeView(GLuint minLevel = 0, GLuint numLevels = -1)
 	{
-		static_assert(NewTexType != TextureType::TEX_2D, "TextureCube: Use the TextureType::TEX_CUBE_{X,Y,Z}_{POS,NEG} instead of TextureType::TEX_2D.")
-		static_assert(/*NewTexType == TextureType::TEX_2D*/		|| NewTexType == TextureType::TEX_2D_ARRAY
+		static_assert(NewTexType != TextureType::TEX_2D, "TextureCube: Use the TextureType::TEX_CUBE_{X,Y,Z}_{POS,NEG} instead of TextureType::TEX_2D.");
+		static_assert(/*NewTexType == TextureType::TEX_2D || */    NewTexType == TextureType::TEX_2D_ARRAY
 			|| NewTexType == TextureType::TEX_CUBE_MAP			|| NewTexType == TextureType::TEX_CUBE_MAP_ARRAY
 			|| detail::IsTextureTypeCubeSide(NewTexType)		, "TextureCube: Incompatible view target.");
 		GLuint minLayers = 0, numLayers = 6;
-		constexpr if (detail::IsTextureTypeCubeSide(NewTexType))
+		if constexpr (detail::IsTextureTypeCubeSide(NewTexType))
 		{
 			minLayers = static_cast<GLuint>(NewTexType) - static_cast<GLuint>(TextureType::TEX_CUBE_X_POS);
 			numLayers = 1;
@@ -166,7 +217,7 @@ public:
 		if (numLevels == -1) numLevels = this->_depth - minLevel;
 		ASSERT(minLevel < this->_depth, "TextureCube: Too large mipmap index.");
 		WARNING(minLevel + numLevels > this->_depth, "TextureCube: Number of mipmap levels must be more than intended. For maximum available mipmap levels, use -1.");
-		constexpr if (detail::IsTextureTypeCubeSide(NewTexType))
+		if constexpr (detail::IsTextureTypeCubeSide(NewTexType))
 			return this->_MakeView<NewInternalFormat, TextureType::TEX_2D>, NewTexType>(minLevel, numLevels, minLayers, numLayers);
 		else
 			return this->_MakeView<NewInternalFormat, NewTexType>(minLevel, numLevels, minLayers, numLayers);
