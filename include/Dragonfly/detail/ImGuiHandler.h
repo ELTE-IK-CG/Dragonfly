0ed86d886dafa82983490d502b5f277917863fbc
Róbert Bán <robert.ban102@gmail.com>
2019-11-17 21:34:45 +0100

    ImGui eventhandler for the Sample class

---
+++ include/Dragonfly/detail/ImGuiHandler.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <SDL/SDL.h>
+#include <ImGui/imgui.h>
+
+namespace df
+{
+
+class ImGuiHandler {
+	static bool HandleKeyUp(const SDL_KeyboardEvent&) {
+		return ImGui::GetIO().WantCaptureKeyboard;
+	}
+	static bool HandleKeyDown(const SDL_KeyboardEvent&) {
+		return ImGui::GetIO().WantCaptureKeyboard;
+	}
+	static bool HandleMouseUp(const SDL_MouseButtonEvent&) {
+		return ImGui::GetIO().WantCaptureMouse;
+	}
+	static bool HandleMouseDown(const SDL_MouseButtonEvent&) {
+		return ImGui::GetIO().WantCaptureMouse;
+	}
+	static bool HandleMouseMotion(const SDL_MouseMotionEvent&) {
+		return ImGui::GetIO().WantCaptureMouse;
+	}
+	static bool HandleMouseWheel(const SDL_MouseWheelEvent&) {
+		return ImGui::GetIO().WantCaptureMouse;
+	}
+};
+
+}
