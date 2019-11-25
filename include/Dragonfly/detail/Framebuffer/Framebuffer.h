eba78bf936f69fec7aae9c2161d194604ba9c0ef
Csabix <csabix.balint@gmail.com>
2019-11-25 15:30:09 +0100

    Framebuffer continued

--- include/Dragonfly/detail/Framebuffer/Framebuffer.h
+++ include/Dragonfly/detail/Framebuffer/Framebuffer.h
@@ -1,6 +1,6 @@
 #pragma once
-#include "../../config.h"
 #include <tuple>
+#include "../../config.h"
 #include "../Texture/Texture.h"
 #include "../Texture/Texture2D.h"
 #include "../Texture/TextureCube.h"
@@ -47,23 +47,89 @@ public:
 	}
 };
 
-template<typename ... Attachements>
-class FrameBufferObject
+namespace detail {
+
+	template<int depth_=-1, int stencil_=-1, int depthstencil_= -1>
+	struct FBO_compile_data{
+		constexpr int depth = depth_;			constexpr int stencil = stencil_;			constexpr int depthstencil = depthstencil_;
+		constexpr bool has_depth = depth != -1;	constexpr bool has_stencil = stencil != -1;	constexpr bool has_depthstencil = depthstencil != -1;
+	};
+}
+
+template<typename compile_data, typename ... Attachements>
+class FramebufferObject : public FrameBufferBase
 {
-	std::tuple<Attachements...> attachements;
-	constexpr bool has_depth = false;
-	constexpr bool has_stencil = false;
-	constexpr bool has_depthstencil = false;
+private:
+	std::tuple<Attachements...> _attachements;
+	GLuint _id;
+	namespace cg = eltecg::ogl::helper;
+private:
+	FramebufferObject(GLuint id, std::tuple<Attachements...> &&attachements):_id(id), _attachements(attachements){}
+
+	void bind() { glBindFramebuffer(GL_FRAMEBUFFER, _id); }
+
+	/*template<typename new_compile_data, typename ... NewAttachements>
+	using CombinedFramebuffer_t = FramebufferObject<detail::FBO_compile_data<
+		compile_data::has_depth			| new_compile_data::has_depth,
+		compile_data::has_stencil		| new_compile_data::has_stencil,
+		compile_data::has_depthstencil	| new_compile_data::has_depthstencil
+	>, Attachements..., NewAttachements...>;*/
+
+	template<typename F>
+	using InternalFormat_AddT = detail::FBO_compile_data<
+		std::is_same_v<F, cg::depth16>			|| std::is_same_v<F, cg::depth32>		|| std::is_same_v<F, cg::depth32F>,
+		std::is_same_v<F, cg::stencil1>			|| std::is_same_v<F, cg::stencil4>		|| std::is_same_v<F, cg::stencil8>	|| std::is_same_v<F, cg::stencil16>,
+		std::is_same_v<F, cg::depth24stencil8>	|| std::is_same_v<F, cg::depth32Fstencil8>>;
+
+	template<typename F>
+	using FramebufferObject_add_Texture2D = FramebufferObject < InternalFormat_AddT<F>, Attachements..., Texture2D<F>>;
+
+	template<typename F>
+	using FramebufferObject_add_Renderbuffer = FramebufferObject < InternalFormat_AddT<F>, Attachements..., Renderbuffer<F>>;
+	
+	constexpr int calc_tuple_idx(int i)
+	{
+		//todo static assert
+		return i + (i > compile_data::depth ? +1 : 0) + (i > compile_data::stencil ? +1 : 0) + (i > compile_data::depthstencil ? +1 : 0);
+	}
+
 public:
+	FramebufferObject() { glCreateFramebuffers(1, &_id); }
+	~FramebufferObject() { if(id != 0) glDeleteFramebuffers(1, &_id); }
+
+	/*template<typename new_compile_data, typename ... NewAttachements>
+	CombinedFramebuffer_t< new_compile_data, NewAttachements...>
+		operator + (const FramebufferObject<NewAttachements...>& o)
+	{
+		CombinedFramebuffer_t< new_compile_data, NewAttachements...> fbo(this->_id);
+		fbo.attache_attachementsments = std::tuple_cat(this->_attachements, o._attachements);
+		_id = 0;
+		return fbo;
+	}*/
+
+	template<typename InternalFormat>
+	FramebufferObject_add_Texture2D<InternalFormat> operator + (const Texture2D<InternalFormat> &tex)
+	{
+		FramebufferObject_add_Texture2D<InternalFormat> fbo(this->_id, std::tuple_cat(this->_attachements, std::make_tuple(tex));
+		this->bind();
+		
+		GLenum attachement = std::
+
+		glFramebufferTexture2D(GL_FRAMEBUFFER, )
+
+		this->_id = 0;
+		return fbo;
+	}
 
 	template<int i>
-	constexpr auto& get() {		//static_assert()
-	 	return std::get<i>(attachements);
+	constexpr typename std::tuple_element<i, std::tuple<Attachements...>>& get() {		//static_assert()
+		static_assert(0 <= i && i < std::tuple_size_v<std::tuple<Attachements...>>, "FramebufferObject: the index is not within the tuple's range.")
+	 	return std::get<i>(_attachements);
 	}
 };
 
 template<typename ... ColorAttachements>
-using FBO = FrameBufferObject<Renderbuffer<>, ColorAttachements...>
+using FBO = FramebufferObject<Renderbuffer<>, ColorAttachements...>;
 
 }
 
