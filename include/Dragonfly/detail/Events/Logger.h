8c8ce9730df6f67d22b1d050adcfff868716db0a
Lázár Zsolt <lazar.zsolt.725@gmail.com>
2020-07-31 03:14:43 +0300

    Added implementation to logging macros

--- include/Dragonfly/detail/Events/Logger.h
+++ include/Dragonfly/detail/Events/Logger.h
@@ -4,103 +4,72 @@
 #include <vector>
 
 namespace df {
-namespace detail {
+	namespace detail {
 
-	class Logger
-	{
-	public:
-		struct Entry {
-			enum class SEVERITY : uint32_t {
-				TRACE	= 0,	//for use during developement
-				DEBUG	= 1,	//for debugging
-				INFO	= 2,
-				HINT	= 3,
-				NOTICE	= 4,
-				WARNING = DF_DETAIL_LOGGER_ENTRY_SEVERITY_WARNING,
-				ALARM	= 6,
-				ERROR	= DF_DETAIL_LOGGER_ENTRY_SEVERITY_ERROR,
-				FATAL	= 8,
-			};
-			enum class TYPE : uint32_t {
-				CHECK,
-				ASSERT,
-				MESSAGE,
-				USER
-			};
-			struct Location	{
-				std::string_view filePath;
-				int line;
-
-			};
-
-			SEVERITY severity;
-			TYPE type;
-			Location location;
-			uint64_t timestamp;
-			std::string_view expression;
-			std::string message; //guideline:
-				//{<modul>["<instance>"]:}* title
-				//long message
-			//example:
-				//Program("MyComputeProgram"):Uniform: Incorrect uniform name
-				//The uniform "cam_eye" of type "vec3" was not part of the compiled shader.
-
-			Entry(SEVERITY severity, Location &&location, std::string_view expression, const std::string &message, TYPE type = TYPE::USER); //todo calc timestamp
-			Entry(SEVERITY severity, Location &&location, std::string_view expression, std::string &&message, TYPE type = TYPE::USER); //todo calc timestamp
-		};
-	private:
-		std::vector<Entry> _entries;
-		Logger() {}	//singleton
-		Logger(const Logger&) = delete;
-	public:
-		static Logger& GetSingleton()
+		class Logger
 		{
-			static Logger log;
-			return log;
-		}
-		inline void AddEntry(Entry&& ent) { _entries.emplace_back(std::move(ent)); }
-		inline void ClearEntries() { _entries.clear(); }
-		inline const std::vector<Entry>& GetEntries() const { return _entries; }
-	}; //class Logger
-	
-	ENUM_CLASS_RELATIONAL_OPERATORS(Logger::Entry::SEVERITY)
+		public:
+			struct Entry {
+				enum class SEVERITY : uint32_t {
+					TRACE = 0,	//for use during developement
+					DEBUG = 1,	//for debugging
+					INFO = 2,
+					HINT = 3,
+					NOTICE = 4,
+					WARNING = DF_DETAIL_LOGGER_ENTRY_SEVERITY_WARNING,
+					ALARM = 6,
+					ERROR = DF_DETAIL_LOGGER_ENTRY_SEVERITY_ERROR,
+					FATAL = 8,
+				};
+				enum class TYPE : uint32_t {
+					CHECK,
+					ASSERT,
+					MESSAGE,
+					USER
+				};
+				struct Location {
+					std::string_view filePath;
+					int line;
 
-}	//namespace df::detail
+				};
 
-extern detail::Logger& Logger; //Global singleton variable, declaration here, definition in a .cpp:
-// df::detail::Logger& df::Logger = df::detail::Logger::GetSingleton();	 
+				SEVERITY severity;
+				TYPE type;
+				Location location;
+				uint64_t timestamp;
+				std::string_view expression;
+				std::string message; //guideline:
+					//{<modul>["<instance>"]:}* title
+					//long message
+				//example:
+					//Program("MyComputeProgram"):Uniform: Incorrect uniform name
+					//The uniform "cam_eye" of type "vec3" was not part of the compiled shader.
 
-using LogEntry_Vec = std::vector<detail::Logger::Entry>;
+				Entry(SEVERITY severity, Location&& location, std::string_view expression, const std::string& message, TYPE type = TYPE::USER); //todo calc timestamp
+				Entry(SEVERITY severity, Location&& location, std::string_view expression, std::string&& message, TYPE type = TYPE::USER); //todo calc timestamp
+			};
+		private:
+			std::vector<Entry> _entries;
+			Logger() {}	//singleton
+			Logger(const Logger&) = delete;
+		public:
+			static Logger& GetSingleton()
+			{
+				static Logger log;
+				return log;
+			}
+			inline void AddEntry(Entry&& ent) { _entries.emplace_back(std::move(ent)); }
+			inline void ClearEntries() { _entries.clear(); }
+			inline const std::vector<Entry>& GetEntries() const { return _entries; }
+		}; //class Logger
 
-}	//namespace df
+		ENUM_CLASS_RELATIONAL_OPERATORS(Logger::Entry::SEVERITY)
 
+	}	//namespace df::detail
 
+	extern detail::Logger& Logger; //Global singleton variable, declaration here, definition in a .cpp:
+	// df::detail::Logger& df::Logger = df::detail::Logger::GetSingleton();	 
 
-//another file
-#include <unordered_map>
-
-namespace df {
-
-class LogManager{
-public:
-	using Hash_Type = uint64_t; //hash from filePath + line + message
-
-	struct LogData {
-		struct Instance{
-			uint64_t frameNumber;
-			uint64_t timestamp; 
-		};
-		detail::Logger::Entry entry;
-		std::vector<Instance> instances;
-	};
-
-private:
-	std::unordered_map<Hash_Type, LogData> data;
-	std::vector<std::pair<Hash_Type, std::size_t>> filteredAndOrdered; //todo filter and order operations
-
-public:
-	void HandleLogger(const LogEntry_Vec&, uint64_t frame_num_);
-	bool AddLogEvent(detail::Logger::Entry);
-};
+	using LogEntry_Vec = std::vector<detail::Logger::Entry>;
 
 }	//namespace df
--- include/Dragonfly/detail/utility.h
+++ include/Dragonfly/detail/utility.h
@@ -19,21 +19,83 @@
     inline T operator>= (T a, T b) { return static_cast<T>( static_cast<std::underlying_type<T>::type>(a) >= static_cast<std::underlying_type<T>::type>(b) ); }
 
 
-//... 0-4
-#define DF_DETAIL_LOGGER_ENTRY_SEVERITY_WARNING 5
-//... 6
-#define DF_DETAIL_LOGGER_ENTRY_SEVERITY_ERROR 7
-//... 8
+#define DF_DETAIL_LOGGER_ENTRY_SEVERITY_TRACE	 0
+#define DF_DETAIL_LOGGER_ENTRY_SEVERITY_DEBUG	 1
+#define DF_DETAIL_LOGGER_ENTRY_SEVERITY_INFO	 2
+#define DF_DETAIL_LOGGER_ENTRY_SEVERITY_HINT	 3
+#define DF_DETAIL_LOGGER_ENTRY_SEVERITY_NOTICE	 4
+#define DF_DETAIL_LOGGER_ENTRY_SEVERITY_WARNING  5
+#define DF_DETAIL_LOGGER_ENTRY_SEVERITY_ALARM	 6
+#define DF_DETAIL_LOGGER_ENTRY_SEVERITY_ERROR	 7
+#define DF_DETAIL_LOGGER_ENTRY_SEVERITY_FATAL	 8
+
+#define DF_DETAIL_LOG_MGR_CONDITION_EQUALS       0
+#define DF_DETAIL_LOG_MGR_CONDITION_NEQUALS      1
+#define DF_DETAIL_LOG_MGR_CONDITION_ABOVE        2
+#define DF_DETAIL_LOG_MGR_CONDITION_ABOVE_EQ     3
+#define DF_DETAIL_LOG_MGR_CONDITION_BELOW        4
+#define DF_DETAIL_LOG_MGR_CONDITION_BELOW_EQ     5
 
 // Macros should not depend on Debug/Release mode directly
 
-#define DF_DETAIL_CHECK_IMPL()		//todo
-#define DF_DETAIL_ASSERT_IMPL()		//todo
-#define DF_DETAIL_MESSAGE_IMPL()	//todo
-
 #define _SEV_ df::detail::Logger::Entry::SEVERITY
+#define _TYPE_ df::detail::Logger::Entry::TYPE
 
-//... 0-4
+#define DF_DETAIL_LOCATION df::detail::Logger::Entry::Location{ __FILE__, __LINE__ }
+
+#define DF_DETAIL_CHECK_IMPL(sev, exp, msg) if(exp) df::Logger.AddEntry(df::detail::Logger::Entry(sev, DF_DETAIL_LOCATION, (exp), (msg), _TYPE_::USER));
+#define DF_DETAIL_ASSERT_IMPL(sev, exp, msg) if(!(exp)) df::Logger.AddEntry(df::detail::Logger::Entry(sev, DF_DETAIL_LOCATION, (exp), (msg), _TYPE_::USER));
+#define DF_DETAIL_MESSAGE_IMPL(sev, msg) df::Logger.AddEntry(df::detail::Logger::Entry(sev, DF_DETAIL_LOCATION, "", (msg), _TYPE_::USER));	
+
+#if DF_LOG_SEVERITY >= DF_DETAIL_LOGGER_ENTRY_SEVERITY_TRACE		// =	_SEV_::TRACE
+#define DF_TRACE_CHECK	(expr, msg)		DF_DETAIL_CHECK_IMPL	(expr, msg, _SEV_::TRACE)
+#define DF_TRACE_ASSERT	(expr, msg)		DF_DETAIL_ASSERT_IMPL	(expr, msg, _SEV_::TRACE)
+#define DF_TRACE_MESSAGE	(msg)			DF_DETAIL_MESSAGE_IMPL	(msg,		_SEV_::TRACE)
+#else
+#define DF_TRACE_CHECK	(expr, msg)		true
+#define DF_TRACE_ASSERT	(expr, msg)
+#define DF_TRACE_MESSAGE	(msg)
+#endif
+
+#if DF_LOG_SEVERITY >= DF_DETAIL_LOGGER_ENTRY_SEVERITY_DEBUG		// =	_SEV_::DEBUG
+#define DF_DEBUG_CHECK	(expr, msg)		DF_DETAIL_CHECK_IMPL	(expr, msg, _SEV_::DEBUG)
+#define DF_DEBUG_ASSERT	(expr, msg)		DF_DETAIL_ASSERT_IMPL	(expr, msg, _SEV_::DEBUG)
+#define DF_DEBUG_MESSAGE	(msg)			DF_DETAIL_MESSAGE_IMPL	(msg,		_SEV_::DEBUG)
+#else
+#define DF_DEBUG_CHECK	(expr, msg)		true
+#define DF_DEBUG_ASSERT	(expr, msg)
+#define DF_DEBUG_MESSAGE	(msg)
+#endif
+
+#if DF_LOG_SEVERITY >= DF_DETAIL_LOGGER_ENTRY_SEVERITY_INFO		// =	_SEV_::INFO
+#define DF_INFO_CHECK	(expr, msg)		DF_DETAIL_CHECK_IMPL	(expr, msg, _SEV_::INFO)
+#define DF_INFO_ASSERT	(expr, msg)		DF_DETAIL_ASSERT_IMPL	(expr, msg, _SEV_::INFO)
+#define DF_INFO_MESSAGE	(msg)			DF_DETAIL_MESSAGE_IMPL	(msg,		_SEV_::INFO)
+#else
+#define DF_INFO_CHECK	(expr, msg)		true
+#define DF_INFO_ASSERT	(expr, msg)
+#define DF_INFO_MESSAGE	(msg)
+#endif
+
+#if DF_LOG_SEVERITY >= DF_DETAIL_LOGGER_ENTRY_SEVERITY_HINT		// =	_SEV_::HINT
+#define DF_HINT_CHECK	(expr, msg)		DF_DETAIL_CHECK_IMPL	(expr, msg, _SEV_::HINT)
+#define DF_HINT_ASSERT	(expr, msg)		DF_DETAIL_ASSERT_IMPL	(expr, msg, _SEV_::HINT)
+#define DF_HINT_MESSAGE	(msg)			DF_DETAIL_MESSAGE_IMPL	(msg,		_SEV_::HINT)
+#else
+#define DF_HINT_CHECK	(expr, msg)		true
+#define DF_HINT_ASSERT	(expr, msg)
+#define DF_HINT_MESSAGE	(msg)
+#endif
+
+#if DF_LOG_SEVERITY >= DF_DETAIL_LOGGER_ENTRY_SEVERITY_NOTICE		// =	_SEV_::NOTICE
+#define DF_NOTICE_CHECK	(expr, msg)		DF_DETAIL_CHECK_IMPL	(expr, msg, _SEV_::NOTICE)
+#define DF_NOTICE_ASSERT	(expr, msg)		DF_DETAIL_ASSERT_IMPL	(expr, msg, _SEV_::NOTICE)
+#define DF_NOTICE_MESSAGE	(msg)			DF_DETAIL_MESSAGE_IMPL	(msg,		_SEV_::NOTICE)
+#else
+#define DF_NOTICE_CHECK	(expr, msg)		true
+#define DF_NOTICE_ASSERT	(expr, msg)
+#define DF_NOTICE_MESSAGE	(msg)
+#endif
 
 #if DF_LOG_SEVERITY >= DF_DETAIL_LOGGER_ENTRY_SEVERITY_WARNING		// =	_SEV_::WARNING
 #define DF_WARNING_CHECK	(expr, msg)		DF_DETAIL_CHECK_IMPL	(expr, msg, _SEV_::WARNING)
@@ -45,7 +107,15 @@
 #define DF_WARNING_MESSAGE	(msg)
 #endif
 
-//... 6
+#if DF_LOG_SEVERITY >= DF_DETAIL_LOGGER_ENTRY_SEVERITY_ALARM		// =	_SEV_::ALARM
+#define DF_ALARM_CHECK	(expr, msg)		DF_DETAIL_CHECK_IMPL	(expr, msg, _SEV_::ALARM)
+#define DF_ALARM_ASSERT	(expr, msg)		DF_DETAIL_ASSERT_IMPL	(expr, msg, _SEV_::ALARM)
+#define DF_ALARM_MESSAGE	(msg)			DF_DETAIL_MESSAGE_IMPL	(msg,		_SEV_::ALARM)
+#else
+#define DF_ALARM_CHECK	(expr, msg)		true
+#define DF_ALARM_ASSERT	(expr, msg)
+#define DF_ALARM_MESSAGE	(msg)
+#endif
 
 #if DF_LOG_SEVERITY >= DF_DETAIL_LOGGER_ENTRY_SEVERITY_ERROR			// =	_SEV_::ERROR
 #define DF_ERROR_CHECK		(expr, msg)		DF_DETAIL_CHECK_IMPL	(expr, msg, _SEV_::ERROR)
@@ -57,6 +127,14 @@
 #define DF_ERROR_MESSAGE	(msg)
 #endif
 
-//... 8
+#if DF_LOG_SEVERITY >= DF_DETAIL_LOGGER_ENTRY_SEVERITY_FATAL			// =	_SEV_::FATAL
+#define DF_FATAL_CHECK		(expr, msg)		DF_DETAIL_CHECK_IMPL	(expr, msg, _SEV_::FATAL)
+#define DF_FATAL_ASSERT		(expr, msg)		DF_DETAIL_ASSERT_IMPL	(expr, msg, _SEV_::FATAL)
+#define DF_FATAL_MESSAGE	(msg)			DF_DETAIL_MESSAGE_IMPL	(msg,		_SEV_::FATAL)
+#else
+#define DF_FATAL_CHECK		(expr, msg)		true
+#define DF_FATAL_ASSERT		(expr, msg)
+#define DF_FATAL_MESSAGE	(msg)
+#endif
 
 #undef _SEV_
